98753f68e5320db8842d83995c18eb3b17bdba7e
mb8 <mlbriones8@gmail.com>
2024-10-31 19:57:41 -0500

    Added Review Section Component. WIP.

--- src/ReviewSection_Component.jsx
+++ src/ReviewSection_Component.jsx
@@ -10,20 +10,22 @@
     function ReviewSectionComponent() {
         return (
             <div>
-                <h2 className="reviews_section">Reviews</h2>
-                <div className="a_review_example">
+                <div className="input_review">
+                    {/*what goes here*/}
+                </div>
+                <div className="review_frame">
                     <div>
-                        <h3>PROFILE NAME</h3>
+                        <p>PROFILE NAME</p>
                     </div>
                     <div className="rating">
-                        
+                        {/*what goes here*/}
                     </div>
                     <div>
-                        <h3>TITLE: Lorem ipsum dolor sit amet</h3>
+                        <p>TITLE: Lorem ipsum dolor sit amet</p>
                     </div>
                     <div>
-                        <h3 className="date_written">DATE WRITTEN</h3>
-                        <h3 className="review_description">DESCRIPTION: Lorem ipsum dolor sit amet, 
+                        <p className="date_written">DATE WRITTEN</p>
+                        <p className="review_description">DESCRIPTION: Lorem ipsum dolor sit amet, 
                             consectetur adipiscing elit. Maecenas convallis sem in ex varius, eget 
                             lacinia turpis placerat. Nam elit ligula, sodales eu odio non, aliquet 
                             ultricies diam. Aenean ullamcorper, ligula sit amet egestas maximus, 
@@ -34,7 +36,7 @@
                             Nulla pretium blandit dui, eget efficitur purus tincidunt ut. Integer ultrices 
                             rutrum leo, eu condimentum lectus laoreet sit amet. Ut luctus massa quis 
                             consequat fermentum. Cras sed metus in magna pellentesque luctus.
-                        </h3>
+                        </p>
                     </div>
                 </div>
            </div>
