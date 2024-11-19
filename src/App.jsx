31eca8225c4ee1bc3e2605f26e0d77c26e06191c
samev <samevangelista07@icloud.com>
2024-11-19 17:48:11 -0600

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import Header from "./Header.jsx"
 import Main from "./Main.jsx"
 import Footer from "./Footer.jsx"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login.jsx";
 
 function App() {
   return (
+    <Router>
     <div className="background">
+      {/* Header always shown */}
       <Header />
-      <Main />
+
+      <Routes>
+        {/* Home page */}
+        <Route path="/" element={<Main/>} />
+
+        {/* Log in page */}
+        <Route path="/login" element={<Login/>}/>
+
+      </Routes>
+      
+      {/* Footer always shown */}
       <Footer />
     </div>
+    </Router>
   );
 }
 
