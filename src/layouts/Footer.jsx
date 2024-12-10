21f2d8df5962345d7b5028069a82a177e259bd6e
samev <samevangelista07@icloud.com>
2024-12-10 17:58:22 -0600

    Structured Project better

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css';
+function Footer() {
+    return(
+        <footer>
+            <p className="footer">© 2024 Small Business Database</p>
+        </footer>
+    );
+}
+export default Footer;
