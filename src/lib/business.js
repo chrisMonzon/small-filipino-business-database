484839096e2a5af908be297ebccb468fe1c1f454
Christopher Monzon <47279306+chrisMonzon@users.noreply.github.com>
2025-04-03 10:23:26 -0500

    Update business.js

--- src/lib/business.js
+++ src/lib/business.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API_URL = 'https://small-filipino-business-database-production.up.railway.app/api/businesses/';
+const API_URL = 'https://small-filipino-business-database-production.up.railway.app/api/businesses/';
 // const API_URL = 'http://localhost:3000/api/businesses/';
-const API_URL = 'http://127.0.0.1:8000/api/businesses/';
+// const API_URL = 'http://127.0.0.1:8000/api/businesses/';
 
 export const fetchBusinesses = async () => {
     try {
