e04f61f0434197b8cb81684e1b7acdf158f95518
Christopher Monzon <47279306+chrisMonzon@users.noreply.github.com>
2025-04-03 10:33:54 -0500

    Update settings.py

--- small_business_backend/small_business_backend/settings.py
+++ small_business_backend/small_business_backend/settings.py
@@ -51,19 +51,19 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',   
 ]
 
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
-MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+# MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+# MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
 
 
 CORS_ALLOWED_ORIGINS = [
