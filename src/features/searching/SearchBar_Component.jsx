52e44b1e7f5a4f7949fc065c5ccbf20aa4db80b6
Christopher Monzon <47279306+chrisMonzon@users.noreply.github.com>
2025-04-04 14:05:56 -0500

    Update SearchBar_Component.jsx

--- src/features/searching/SearchBar_Component.jsx
+++ src/features/searching/SearchBar_Component.jsx
@@ -1,31 +1,61 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./SearchBar_Component.css";
 
 function SearchBarComponent({ onSendSearchQuery }) {
+  const [query, setQuery] = useState("");
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
     
-    useEffect(() => {
-        const searchInput = document.querySelector(".search-input");
+      onSendSearchQuery(e.target.value.trim().replace(/ +(?= )/g,''));
+    }
+  };
 
-        searchInput.addEventListener("keydown", (e) => {
-            const enterKey = 13
-            if (e.keyCode != enterKey) return;
-
-            let value = (e.target.value).trim().replace(/ +(?= )/g,'');
-            // console.log("Value: ", value);
-            onSendSearchQuery(value);
-        });
-    });
-
-    return(
-        <div className="searchbar">
-            <span class="search-icon material-symbols-outlined"></span>
-            <input class="search-input" type="search" placeholder="🔍 Find a small business to support!"/>
-        </div>
-    );   
+  return (
+    <div className="searchbar">
+      <span className="search-icon material-symbols-outlined"></span>
+      <input
+        className="search-input"
+        type="search"
+        placeholder="🔍 Find a small business to support!"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
 }
 
 export default SearchBarComponent;
 
+// import { useEffect, useState } from "react";
+// import "./SearchBar_Component.css";
+
+// function SearchBarComponent({ onSendSearchQuery }) {
+    
+//     useEffect(() => {
+//         const searchInput = document.querySelector(".search-input");
+
+//         searchInput.addEventListener("keydown", (e) => {
+//             const enterKey = 13
+//             if (e.keyCode != enterKey) return;
+
+//             let value = (e.target.value).trim().replace(/ +(?= )/g,'');
+//             // console.log("Value: ", value);
+//             onSendSearchQuery(value);
+//         });
+//     });
+
+//     return(
+//         <div className="searchbar">
+//             <span class="search-icon material-symbols-outlined"></span>
+//             <input class="search-input" type="search" placeholder="🔍 Find a small business to support!"/>
+//         </div>
+//     );   
+// }
+
+// export default SearchBarComponent;
 
 
-// I "borrowed" this guy's code: https://www.youtube.com/watch?v=f6ocDCkCmhM
+
+// // I "borrowed" this guy's code: https://www.youtube.com/watch?v=f6ocDCkCmhM
